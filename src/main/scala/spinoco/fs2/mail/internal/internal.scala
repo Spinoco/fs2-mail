f38cdeee4cc3097ae2d1062dfd9ee1cf160d7d7e
Adam Chlupacek <adam@chlupacek.cz>
2018-02-21 10:57:41 +0100

    Addressing comments.

--- src/main/scala/spinoco/fs2/mail/internal/internal.scala
+++ src/main/scala/spinoco/fs2/mail/internal/internal.scala
@@ -7,16 +7,15 @@ import fs2.util.syntax._
 package object internal {
 
   /**
-    * Behaves like [[Stream.takeThrough]], but even if down stream ends earlier than the `toggle`
-    * results into false. This will ensure that the [[Stream]] will not end before we get false from the `toggle`.
+    * Behaves like [[Stream.takeThrough]], but resulting stream will not end until `f` yields to false,
+    * draining all elements where `f` still yields to true.
     *
     * This is done by running the source stream in parallel to the consume stream. The messages read from the
     * source stream are then published into a synchronous queue, that blocks until the down stream reads from it.
     *
     * Once downstream finishes this synchronous queue is read until exhaustion marked by [[None]].
-    *
     */
-  def takeThroughDrain[F[_], A](toggle: A => Boolean)(
+  def takeThroughDrain[F[_], A](predicate: A => Boolean)(
     implicit F: Async[F]
   ): Pipe[F, A, A] = { source =>
     Stream.eval(fs2.async.synchronousQueue[F, Option[A]]).flatMap{ feedQueue =>
@@ -25,7 +24,7 @@ package object internal {
     // the down stream consumed all data available to it.
     Stream.eval(F.refOf(false)).flatMap{ finishedRef =>
       Stream.eval(F.start(
-        source.takeThrough(toggle)
+        source.takeThrough(predicate)
         .evalMap( a => feedQueue.enqueue1(Some(a)))
         .onFinalize(finishedRef.setPure(true) >> feedQueue.enqueue1(None))
         .run
